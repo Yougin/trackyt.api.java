66d721ac545c0ebbef1d3b25c8808e3b5f8c9505
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-02-23 21:29:52 +0200

    Empty models created

---
+++ src/trackyt/api/java/responses/AuthenticationResponse.java
@@ -0,0 +1,7 @@
+package trackyt.api.java.responses;
+
+import trackyt.api.java.responses.BaseResponse;
+
+public class AuthenticationResponse extends BaseResponse {
+
+}
---
+++ src/trackyt/api/java/responses/GetAllTasksResponse.java
@@ -0,0 +1,7 @@
+package trackyt.api.java.responses;
+
+import trackyt.api.java.responses.BaseResponse;
+
+public class GetAllTasksResponse extends BaseResponse {
+
+}
