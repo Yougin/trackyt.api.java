ccc35fbcb71d901f388442cf59833f2cb419778d
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-02-26 23:56:16 +0200

    Model created

--- src/trackyt/api/java/responses/GetAllTasksResponse.java
+++ src/trackyt/api/java/responses/GetAllTasksResponse.java
@@ -1,7 +1,27 @@
 package trackyt.api.java.responses;
 
-import trackyt.api.java.responses.BaseResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+import trackyt.api.java.models.Task;
 
 public class GetAllTasksResponse extends BaseResponse {
+	public GetAllTasksData data;
 
+	public List<Task> getTasksList() {
+		return data.getTasksList();
+	}
+}
+
+class GetAllTasksData {
+	
+	List<Task> tasksList = new ArrayList<Task>();
+	
+	public GetAllTasksData() {
+		
+	}
+	
+	public List<Task> getTasksList() {
+		return tasksList;
+	}
 }
