796e482916389b59ccef39ccfaf9b9adec9b301b
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-02-26 15:26:32 +0200

    Methods names corrected

--- src/trackyt/api/java/adapter/ApiV11Adapter.java
+++ src/trackyt/api/java/adapter/ApiV11Adapter.java
@@ -10,51 +10,51 @@ import trackyt.api.java.models.Task;
 public class ApiV11Adapter implements TrackytApiAdapter {
 
 	@Override
-	public ApiToken Authenticate(String email, String password)
+	public ApiToken authenticate(String email, String password)
 			throws NotAuthenticatedException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<Task> GetAllTasks(ApiToken token) throws Exception {
+	public List<Task> getAllTasks(ApiToken token) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Task AddTask(ApiToken token, String description)
+	public Task addTask(ApiToken token, String description)
 			throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public int DeleteTask(ApiToken apiToken, int taskId) throws Exception {
+	public int deleteTask(ApiToken apiToken, int taskId) throws Exception {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
-	public Task StartTask(ApiToken apiToken, int taskId) throws Exception {
+	public Task startTask(ApiToken apiToken, int taskId) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public Task StopTask(ApiToken apiToken, int taskId) throws Exception {
+	public Task stopTask(ApiToken apiToken, int taskId) throws Exception {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public void StartAll(ApiToken apiToken) throws Exception {
+	public void startAll(ApiToken apiToken) throws Exception {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void StopAll(ApiToken apiToken) throws Exception {
+	public void stopAll(ApiToken apiToken) throws Exception {
 		// TODO Auto-generated method stub
 		
 	}
