aa522a14abb58212097ad9671947a7ee9ceb600c
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-02-26 23:14:33 +0200

    Model defined

--- src/trackyt/api/java/responses/AuthenticationResponse.java
+++ src/trackyt/api/java/responses/AuthenticationResponse.java
@@ -4,4 +4,26 @@ import trackyt.api.java.responses.BaseResponse;
 
 public class AuthenticationResponse extends BaseResponse {
 	
+	public Data data;
+	
+	public String getApiToken() {
+		return data.getApiToken();
+	}
+	
+}
+
+class Data {
+	public String apiToken;
+
+	public Data() {
+		
+	}
+	public String getApiToken() {
+		return apiToken;
+	}
+
+	public void setApiToken(String apiToken) {
+		this.apiToken = apiToken;
+	}
+	
 }
