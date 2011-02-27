c70cc5360b4c64164b5143715e6fc786302caf93
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-02-27 11:14:18 +0200

    StopTaskResponseData class defined

--- src/trackyt/api/java/responses/StopTaskResponse.java
+++ src/trackyt/api/java/responses/StopTaskResponse.java
@@ -1,7 +1,19 @@
 package trackyt.api.java.responses;
 
-import trackyt.api.java.responses.BaseResponse;
+import trackyt.api.java.models.Task;
 
 public class StopTaskResponse extends BaseResponse{
+	StopTaskResponseData data;
+	
+	public Task getTask() {
+		return data.getTask();
+	}
+}
 
+class StopTaskResponseData {
+	Task task;
+	
+	public Task getTask() {
+		return task;
+	}
 }
