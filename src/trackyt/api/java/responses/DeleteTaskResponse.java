5793c77700be62d1285e18e8abd5de93f4b080f3
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-02-27 11:04:07 +0200

    DeleteTaskResponseData class implemented

--- src/trackyt/api/java/responses/DeleteTaskResponse.java
+++ src/trackyt/api/java/responses/DeleteTaskResponse.java
@@ -3,5 +3,20 @@ package trackyt.api.java.responses;
 import trackyt.api.java.responses.BaseResponse;
 
 public class DeleteTaskResponse extends BaseResponse {
+	
+	DeleteTaskResponseData data;
+	
+	public int getTaskId() {
+		return data.getTaskId();
+	}
+	
+}
 
+class DeleteTaskResponseData {
+	
+	int id;
+	
+	public int getTaskId() {
+		return id;
+	}
 }
