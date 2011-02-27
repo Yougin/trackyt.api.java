e13312aac8680ddbbb38653756d52852702d7480
Eugene Beletskiy <ebeletskiy@gmail.com>
2011-02-27 11:10:40 +0200

    StartTaskResponseData class defined

--- src/trackyt/api/java/responses/StartTaskResponse.java
+++ src/trackyt/api/java/responses/StartTaskResponse.java
@@ -1,7 +1,19 @@
 package trackyt.api.java.responses;
 
-import trackyt.api.java.responses.BaseResponse;
+import trackyt.api.java.models.Task;
 
 public class StartTaskResponse extends BaseResponse{
+	StartTaskResponseData data;
+	
+	public Task getTask() {
+		return data.getTask();
+	}
+}
 
+class StartTaskResponseData {
+	Task task;
+	
+	public Task getTask() {
+		return task;
+	}
 }
